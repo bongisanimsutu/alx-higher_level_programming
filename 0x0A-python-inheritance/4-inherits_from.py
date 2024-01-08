75b104351d7f1645e7bdbc002034f6b567eb2993
bongisanimsutu <97881704+bongisanimsutu@users.noreply.github.com>
2024-01-08 14:53:02 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module containing is_kind_of_class method"""
 
-def inherits_from(obj, a_class):
-    """Method that return True if an object is an instance of a class
-    that inherited from"""
 
-    return False if type(obj) is a_class else isinstance(obj, a_class)
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of, or if the
+    object is an instance of a class that inherited from
+    the specified class ; otherwise False """
+    return isinstance(obj, a_class)
