10bd3fedad86f33d64293559000b85f0282e4c73
bongisanimsutu <bongisanimsutu@gmail.com>
2024-01-08 04:12:58 -0800

    Testing

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method for calculated area"""
+
+        raise Exception("area() is not implemented")
